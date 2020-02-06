3b56556c31b91dc38c07a3cadeb9a3c177660017
nazarethbrown <49081904+nazarethbrown@users.noreply.github.com>
2020-02-06 18:14:31 -0500

    integer benchmark
    This portion of the code is the creation of the integer benchmark. it will include the addition, multiplication, and division of integers.

---
+++ Benchmark.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void integer_benchmark(){
+   for(long i = 0; i < 100000000000; i++){
+      long n = 0;
+      n++;
+    }
+}
+
+int main() {
+  
+  cout << "Hello World!\n";
+}
