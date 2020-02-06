e7b58df0a366a9787cc4ba65ddfc846b43e9b5f5
nazarethbrown <49081904+nazarethbrown@users.noreply.github.com>
2020-02-06 18:49:28 -0500

    multiplication of integer
    tests the integer multiplication speed by multiply m * 6 5x10^10 times

--- Benchmark.cpp
+++ Benchmark.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 void integer_benchmark(){
+   long n = 0;
    for(long i = 0; i < 100000000000; i++){
-      long n = 0;
       n++;
+    }
+   long m = 6;
+   for(long j = 0; j < 50000000000; j++){
+      m = 6 * m;
     }
 }
 
