ff3a8c6a950b347598ef91c52915c9be794466b5
nazarethbrown <49081904+nazarethbrown@users.noreply.github.com>
2020-02-06 22:56:36 -0500

    using time.h to record time
    copy & pasted everything from integer function into float function & changed values to floats. used time in at beginning and end of each function to test each benchmark.

--- Benchmark.cpp
+++ Benchmark.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <time.h>
+#include <fstream>
 using namespace std;
 
+
+
+
 void integer_benchmark(){
+  time_t start, stop;
+  time(&start);
   long long n = 0;
    for(long long i = 0; i < 100000000000; i++){
       n++;
@@ -14,8 +23,36 @@ void integer_benchmark(){
     for(long long k = 0; k < 20000000000; k++){
       p = p/2;
     }
+    time(&stop);
+    double time;
+    time = double(stop - start);
 }
+void float_benchmark(){
+   time_t start, stop;
+  time(&start);
+  double double n = 0.0;
+   for(double double i = 0; i < 100000000000; i++){
+      n++;
+    }
+    double double m = 2.0;
+    for(double double j = 0; j < 50000000000; j++){
+      m = 6.0 * m;
+    }
+    double double p = 63748930239392034.90394458;
+    for(double double k = 0; k < 20000000000; k++){
+      p = p/2.0;
+    }
+    time(&stop);
+    double time;
+    time = double(stop - start);
 
+}
+void memory_benchmark(){
+
+}
+void file_benchmark(){
+  
+}
 int main() {
   
   cout << "Hello World!\n";
